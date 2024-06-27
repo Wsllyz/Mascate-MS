754e87140a9eb66c18c944cb29e7a44fecd1025a
Weslley Santana <westvsantana@gmail.com>
2024-06-27 05:08:26 -0300

    updateProject

--- funcionario/views.py
+++ funcionario/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-
+from .models import DadosFuncionario
 # Create your views here.
 def adms(request):
+    nova_pessoa = DadosFuncionario(nome_funcionario='Cleber', idade_funcionario=22, telefone_funcionario='81996549125')
+    nova_pessoa.save()
     return render(request, 'funcionarioLogin.html')
 
 def funcionarioPrincipal_view(request):
